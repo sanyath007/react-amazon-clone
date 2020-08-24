2704223d119cab9e61dfc676c2519c1ef99fad87
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-24 11:10:12 +0700

    Create firebase.js for firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBP5UFsKjQtxc2Y3VMKvYcPMUMU8QsV0jU",
+  authDomain: "clone-2bc98.firebaseapp.com",
+  databaseURL: "https://clone-2bc98.firebaseio.com",
+  projectId: "clone-2bc98",
+  storageBucket: "clone-2bc98.appspot.com",
+  messagingSenderId: "237374920173",
+  appId: "1:237374920173:web:ebd7eccc1e0207d55a83e4"
+};
