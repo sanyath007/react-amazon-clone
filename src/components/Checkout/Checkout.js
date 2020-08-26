afd9f991946ded2579ed8d34db5d80bf1a2c1707
Sanyath Thammawong <sanyath007@gmail.com>
2020-08-26 22:01:35 +0700

    Style a little bit Checkout and CheckoutProduct

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -8,30 +8,36 @@ function Checkout() {
 
   return (
     <div className="checkout">
-      <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+      <div className="checkout__left">
+        <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
 
-      {basket?.length === 0 ? (
-        <div>
-          <h2>Your Shopping Basket is empty</h2>
-          <p>
-            You have no items in your basket. To buy one or more items, click "Add to Basket" next to the item.
-          </p>
-        </div>
-      ) : (
-        <div>
-          <h2 className="checkout__title">Your Shopping Basket</h2>
-          {basket.map((item, index) => (
-            <CheckoutProduct
-              id={item._id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-              key={index}
-            />
-          ))}
-        </div>
-      )}
+        {basket?.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+            <p>
+              You have no items in your basket. To buy one or more items, click "Add to Basket" next to the item.
+            </p>
+          </div>
+        ) : (
+          <div>
+            <h2 className="checkout__title">Your Shopping Basket</h2>
+            {basket.map((item, index) => (
+              <CheckoutProduct
+                id={item._id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+                key={index}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="checkout__right">
+        <h1>Subtotal</h1>
+      </div>
     </div>
   )
 }
